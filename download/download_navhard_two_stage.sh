43ff53a5ab8233002567e68c4bd3c554b61e75b8
magis-techno <magis.techno@gmail.com>
2025-07-22 20:14:30 +0800

    update

--- download/download_navhard_two_stage.sh
+++ download/download_navhard_two_stage.sh
@@ -1,9 +1,21 @@
-wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.2_navhard_two_stage_curr_sensors.tar.gz
-wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.2_navhard_two_stage_hist_sensors.tar.gz
-wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.2_navhard_two_stage_scene_pickles.tar.gz
+#!/bin/bash
+
+# OBS base path for navsim-v2 data
+OBS_BASE_PATH="training/yw-ads-training-gy1/data/opensource/huggingface/dataset/OpenDriveLab/OpenScene/main/navsim-v2"
+
+# Download navhard two stage data from OBS
+echo "Downloading navhard two stage data from OBS..."
+di-mc cp "${OBS_BASE_PATH}/navsim_v2.2_navhard_two_stage_curr_sensors.tar.gz" .
+di-mc cp "${OBS_BASE_PATH}/navsim_v2.2_navhard_two_stage_hist_sensors.tar.gz" .
+di-mc cp "${OBS_BASE_PATH}/navsim_v2.2_navhard_two_stage_scene_pickles.tar.gz" .
+
+echo "Extracting navhard two stage files..."
 tar -xzvf navsim_v2.2_navhard_two_stage_curr_sensors.tar.gz
 tar -xzvf navsim_v2.2_navhard_two_stage_hist_sensors.tar.gz
 tar -xzvf navsim_v2.2_navhard_two_stage_scene_pickles.tar.gz
+
 rm navsim_v2.2_navhard_two_stage_curr_sensors.tar.gz
 rm navsim_v2.2_navhard_two_stage_hist_sensors.tar.gz
 rm navsim_v2.2_navhard_two_stage_scene_pickles.tar.gz
+
+echo "Navhard two stage dataset download completed!"
--- download/download_warmup_two_stage.sh
+++ download/download_warmup_two_stage.sh
@@ -1,3 +1,12 @@
-wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.2_warmup_two_stage.tar.gz
+#!/bin/bash
+
+# OBS base path for navsim-v2 data
+OBS_BASE_PATH="training/yw-ads-training-gy1/data/opensource/huggingface/dataset/OpenDriveLab/OpenScene/main/navsim-v2"
+
+# Download warmup two stage data from OBS
+echo "Downloading navsim_v2.2_warmup_two_stage.tar.gz from OBS..."
+di-mc cp "${OBS_BASE_PATH}/navsim_v2.2_warmup_two_stage.tar.gz" .
 tar -xzvf navsim_v2.2_warmup_two_stage.tar.gz
 rm navsim_v2.2_warmup_two_stage.tar.gz
+
+echo "Warmup two stage dataset download completed!"
